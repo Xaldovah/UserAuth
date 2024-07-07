f07e52bc29bcee13499dc999c128ae12df9e9d3b
xaldovah <dxaldovah@gmail.com>
2024-07-07 18:39:38 +0300

    updated settings file

--- Authen/Authen/settings.py
+++ Authen/Authen/settings.py
@@ -10,12 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PORT = os.environ.get('PORT', '8000')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
