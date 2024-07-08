5a60dfca08eac3ddaac17e19b4f5688054ba9983
Xaldovah <dxaldovah@gmail.com>
2024-07-08 17:05:21 +0000

    Pytest Configuration files

--- Authen/conftest.py
+++ Authen/conftest.py
@@ -1,16 +1,6 @@
-import pytest
+import os
+import django
 from django.conf import settings
 
-
-@pytest.fixture(scope='session')
-def django_db_setup():
-    settings.DATABASES['default'] = {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'postgres',
-            'PASSWORD': 'Postgres236',
-            'HOST': 'localhost',
-            'PORT': '5432',
-            'ATOMIC_REQUESTS': True,
-    }
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Authen.settings'
+django.setup()
